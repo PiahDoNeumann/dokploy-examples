cecaeaa384c5e4dd016f8d19d0308e0919df7e37
Valdecir Neumann <piahdoneumann@gmail.com>
2025-01-25 14:59:24 -0300

    Tentando dinovo.

--- patobragado/src/App.vue
+++ patobragado/src/App.vue
@@ -1,6 +1,21 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+
+import Parceiros from './components/Parceiros.vue'
+import AppHeader from './components/AppHeader.vue'
+import AppFooter from './components/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Parceiros,
+    AppHeader,
+    AppFooter,
+    HelloWorld,
+    TheWelcome
+  }
+}
 </script>
 
 <template>
@@ -15,9 +30,23 @@ import TheWelcome from './components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+
+  <app-header />
+  <Parceiros />
+  <app-footer />
+
 </template>
 
 <style scoped>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
 header {
   line-height: 1.5;
 }
